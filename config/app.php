c219bbaa077d634698bf5ae7c6f0d0e89e3fead3
EJyoungc <nerst@live.com>
2024-12-05 06:23:59 +0200

    main

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'MAASMS'),
 
     /*
     |--------------------------------------------------------------------------
@@ -39,7 +39,7 @@ return [
     |
     */
 
-    'debug' => (bool) env('APP_DEBUG', false),
+    'debug' => (bool) env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -97,7 +97,7 @@ return [
 
     'cipher' => 'AES-256-CBC',
 
-    'key' => env('APP_KEY'),
+    'key' => env('base64:SkVSSXuFqVzD2UHNeLankzHZEXcHrT/cfZ1WZgaaF04='),
 
     'previous_keys' => [
         ...array_filter(
