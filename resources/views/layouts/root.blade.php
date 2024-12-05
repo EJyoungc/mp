829f3123bb2773bb0fc019908a065f5cfba86fde
EJyoungc <nerst@live.com>
2024-12-05 07:42:03 +0200

    main

--- resources/views/layouts/root.blade.php
+++ resources/views/layouts/root.blade.php
@@ -86,9 +86,10 @@
             });
             // });
         </script>
-        <script src="{{ asset('plugins/bootstrap/js/bootstrap.bundle.min.js') }}" ></script>
+        
 
         <script src="{{ asset('plugins/jquery/jquery.min.js') }}" ></script>
+        <script src="{{ asset('plugins/bootstrap/js/bootstrap.bundle.min.js') }}" ></script>
         <script src="{{ asset('dist/js/adminlte.min.js') }}" ></script>
         
 </body>
