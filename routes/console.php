dbbbba10eb1c359057a102266746781a6a95fd8e
EJyoungc <nerst@live.com>
2025-12-17 19:56:52 +0200

    commands

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,17 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Schedule::call(function () {
+    \Log::info('Hostinger cron is running every minute');
+})->everyMinute();
+
+// Schedule::command('app:check-messages')
+//     ->everyMinute()
+//     ->withoutOverlapping()
+//     ->appendOutputTo(storage_path('logs/sms-cron.log'));
