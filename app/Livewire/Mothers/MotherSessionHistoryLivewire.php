4130a095643897af5bc096d813dc47562ff0a242
EJyoungc <nerst@live.com>
2026-05-06 10:32:42 +0200

    update

--- app/Livewire/Mothers/MotherSessionHistoryLivewire.php
+++ app/Livewire/Mothers/MotherSessionHistoryLivewire.php
@@ -53,7 +53,7 @@ class MotherSessionHistoryLivewire extends Component
         $sms = $AT->sms();
 
         $result = $sms->send([
-            'to' => '+2650884348727',
+            'to' => '+265884348727',
             'message' => 'this is a maasms test',
             'from' => 'Maasms',
         ]);
